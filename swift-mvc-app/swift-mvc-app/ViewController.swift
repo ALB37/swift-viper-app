0e2e8ea412e017a4f52eb457963b5a76cffe9831
Andrew Bloom <blooma116@gmail.com>
2018-04-14 17:01:39 -0700

    setting up basic ui

---
+++ swift-mvc-app/swift-mvc-app/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  swift-mvc-app
+//
+//  Created by Andrew Bloom on 4/14/18.
+//  Copyright © 2018 Andrew Bloom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
